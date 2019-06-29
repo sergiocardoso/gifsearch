b3e284b26b1b24fec73710bd92216fb8e0b649f9
Sérgio Cardoso <contato@flok.in>
2019-06-29 10:11:47 +0200

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:gifsearch/ui/homepage.dart';
+import 'package:gifsearch/ui/gif_page.dart';
+
+void main() {
+  runApp(
+      MaterialApp(home: HomePage(), theme: ThemeData(hintColor: Colors.white)));
+}
